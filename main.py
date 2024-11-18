f61238a25cfaf2cd3811632bd7e35e8733ac1772
AlexisS <alexis.sacre98@gmail.com>
2024-11-18 01:28:03 +0100

    Revert "Db script added"
    This reverts commit 417663649d13f2e944507ea343240bdec77cc60c.

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from dotenv import load_dotenv
-import os
 
 
 def print_hi(name):
@@ -13,10 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    load_dotenv()
     print_hi('PyCharm')
-    print(os.getenv("db_username"))
-    print(os.getenv("db_password"))
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
