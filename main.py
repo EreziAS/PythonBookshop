417663649d13f2e944507ea343240bdec77cc60c
AlexisS <alexis.sacre98@gmail.com>
2024-11-18 01:26:24 +0100

    Db script added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from dotenv import load_dotenv
+import os
 
 
 def print_hi(name):
@@ -11,6 +13,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    load_dotenv()
     print_hi('PyCharm')
+    print(os.getenv("db_username"))
+    print(os.getenv("db_password"))
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
